cb51ab805f50432e22732ee5e6b1c8b9d9f2a57f
dali <dali.meliodas.mdb@gmail.com>
2025-03-09 17:46:59 +0100

    initial commit: set up and data base

---
+++ travel_agency/main/serializers.py
@@ -0,0 +1,103 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Customer
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'password']
+
+    def validate(self, data):
+        email = data.get("email")
+        username = data.get("username")
+        phone_number = data.get("phone_number")
+        password = data.get('password')
+
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({"email": "A user with this email already exists."})
+
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError({"username": "This username is already taken."})
+
+        if User.objects.filter(phone_number=phone_number).exists():
+            raise serializers.ValidationError({"phone_number": "This phone number is already registered."})
+
+        def is_strong_password(password):
+            if (
+                len(password) < 8 or
+                not any(char.islower() for char in password) or
+                not any(char.isupper() for char in password)
+            ):
+                return False
+            return True
+
+        if not is_strong_password(password):
+            raise serializers.ValidationError({'password': 'The password you have provided is too weak.'})
+
+        return data
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Customer
+        fields = [
+            'user', 'first_name', 'last_name', 'country', 'city', 'birthdate',
+            'profile_picture', 'gender', 'favorite_currency'
+        ]
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_ser = UserSerializer(data=user_data)
+
+        if user_ser.is_valid():
+            user = user_ser.save()
+            customer = Customer.objects.create(user=user, **validated_data)
+            return customer
+        else:
+            raise serializers.ValidationError(user_ser.errors)
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', None)
+        user = instance.user
+
+        # Update the User instance
+        if user_data:
+            user_ser = UserSerializer(user, data=user_data, partial=True)
+            if user_ser.is_valid():
+                user_ser.save()
+            else:
+                raise serializers.ValidationError(user_ser.errors)
+
+        # Update the Customer instance
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
+
+
+
+class AdminSerializer(serializers.Serializer):
+    pass
---
+++ travel_agency/main/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view,APIView,permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import CustomerSerializer
+from django.contrib.auth import authenticate,get_user_model
+from rest_framework.permissions import IsAuthenticated
+
+
+
+User = get_user_model()
+
+
+
+@api_view(['GET'])
+def home(request):
+    return Response({"hello world" : "hello world"},status=status.HTTP_202_ACCEPTED)
+
+
+
+@api_view(['POST'])
+def register(request):
+    user_ser = CustomerSerializer(data=request.data)
+    if user_ser.is_valid():
+        customer = user_ser.save()
+        refresh = RefreshToken.for_user(customer.user)
+
+        context = {
+            'details' : user_ser.data,
+            'success' : 'user created successfully',
+            'refresh' : str(refresh),
+            'access' :  str(refresh.access_token)
+        }
+        return  Response(context, status=status.HTTP_201_CREATED)
+    return Response(user_ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['POST'])
+def login(request):
+    username_or_email = request.data.get('email')
+    password = request.data.get('password')
+
+    if not username_or_email or not password:
+        return Response({"error": "Email/Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(email=username_or_email, password=password)
+
+    if not user:
+        # Try authenticating by email
+        try:
+            email = User.objects.get(username=username_or_email).email
+            user = authenticate(email=email, password=password)
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+    if user:
+        refresh = RefreshToken.for_user(user)
+        tokens = {
+            'refresh' : str(refresh),
+            'access' : str(refresh.access_token)
+        }
+        return Response(tokens, status=status.HTTP_200_OK)
+
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    return Response({"success":"you are allowed to view this page"})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    refresh_token = request.data['refresh']
+    if not refresh_token:
+        return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        token = RefreshToken(refresh_token)
+        token.blacklist()  # Blacklist the refresh token
+        return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
+    except :
+        return Response({'error': 'something happened'}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['post'])
+def get_refresh(request):
+    try:
+        access = str(  RefreshToken(request.data['refresh']).access_token  )
+        return Response({'access':access})
+    except:
+        return Response({'error':'the given token has been blacklisted'})
