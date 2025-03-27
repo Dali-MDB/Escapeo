ba02c00c42c7fd3b54c57b9dd7749a7054b74bd7
Neil Zitouni <nn_zitouni@esi.dz>
2025-03-27 02:04:51 +0100

    updated the routing to handle group messages

--- backend/travel_agency/routing.py
+++ backend/travel_agency/routing.py
@@ -3,13 +3,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from backend.chat.consumers import PrivateChatConsumer  # Ensure the correct import path
+from backend.chat.consumers import PrivateChatConsumer, GroupChatConsumer
 
 application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
-                re_path(r"^messages/(?P<recipient>[\w.@+-]+)/$", PrivateChatConsumer.as_asgi()),  
+                re_path(r"^ws/privatemessages/(?P<recipient>[a-zA-Z0-9_.0+-]+)/$", PrivateChatConsumer.as_asgi()),  
+                re_path(r"^ws/groupmessages/(?P<group_id>[0-9a-f-]+)/$",GroupChatConsumer.as_asgi()),
             ])
         )
     ),
