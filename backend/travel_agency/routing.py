fbda98fbace796f162f20368b90e341e28a15cee
Neil Zitouni <nn_zitouni@esi.dz>
2025-03-28 04:33:38 +0100

    fixed the routing file

--- backend/travel_agency/routing.py
+++ backend/travel_agency/routing.py
@@ -9,7 +9,7 @@ application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
-                re_path(r"^ws/privatemessages/(?P<recipient>[a-zA-Z0-9_.0+-]+)/$", PrivateChatConsumer.as_asgi()),  
+                re_path(r"^ws/privatemessages/(?P<username>[a-zA-Z0-9_.0+-]+)/$", PrivateChatConsumer.as_asgi()),  
                 re_path(r"^ws/groupmessages/(?P<group_id>[0-9a-f-]+)/$",GroupChatConsumer.as_asgi()),
             ])
         )
