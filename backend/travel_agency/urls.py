e5252f7c5a6d2611a0e03bd27335d23a5c299513
dali <dali.meliodas.mdb@gmail.com>
2025-04-03 00:09:41 +0100

    added automatic schema and documentation

--- backend/travel_agency/urls.py
+++ backend/travel_agency/urls.py
@@ -3,11 +3,15 @@ from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from main.views import path_not_found
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
     path('chatbot/',include('chatbot.urls')),
     path('reservation/',include('reservation.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema')),
     re_path(r"^(?P<path>.*)$",path_not_found ),  
 ]+  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
