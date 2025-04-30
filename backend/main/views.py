240794af26c3b5b2d534fdc065ad4c6d86f23400
Dali-MDB <155333730+Dali-MDB@users.noreply.github.com>
2025-04-30 09:48:31 +0100

    added get trips by country (location)

--- backend/main/views.py
+++ backend/main/views.py
@@ -989,6 +989,12 @@ def get_user_info(request):
         status=status.HTTP_200_OK
     )
 
+@api_view(['GET'])
+def get_trips_for_country(request,country):
+    trips = Trip.objects.filter(destination__icontains = country)
+    trips_ser = TripSerializer(trips,many=True)
+    return Response(trips_ser.data,status=status.HTTP_200_OK)
+
 
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def path_not_found(request, path=None):
