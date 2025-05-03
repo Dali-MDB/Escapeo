2d73d80f80a8a02994a86861cd7bec81545a7432
dali <dali.meliodas.mdb@gmail.com>
2025-05-03 20:50:20 +0100

    added end points to view managed trips and history

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('password_reset_confirm/', view=views.ConfirmPasswordResetCodeView.as_view(), name='password-reset-confirm'),
     path('update_profile_picture/',view=views.update_profile_picture,name='update-profile-picture'),
     path('my_account/',view=views.MyAccount.as_view(),name='my-account'),
+    path('get_my_managed_trips/',view=views.get_my_managed_trips,name='get_my_managed_trips'),
+    path('get_my_travel_history/',view=views.get_my_travel_history,name='get_my_travel_history'),
+
 ]
