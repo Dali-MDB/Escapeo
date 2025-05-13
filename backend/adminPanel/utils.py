f4e9dedd76c23baf2aba92dd5fb8b005599baefe
mKharchi <kh.merouane.abderrahmane@gmail.com>
2025-05-13 10:25:19 +0100

    test

--- backend/adminPanel/utils.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]  # First IP in the list
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
